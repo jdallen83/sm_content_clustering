d294cda39b81f1007081f31d46df5843ab578897
jeff <jeff@jeff.jeff>
2021-11-19 13:40:38 -0500

    Creating minimal setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='sm_content_clustering',
+    version='0.1',
+    description='A Python module for clustering creators of social media content into networks',
+    url='http://github.com/jdallen83/sm_content_clustering',
+    author='Jeff Allen',
+    author_email='jeff@integrityinstitute.org',
+    license='MIT',
+    packages=['sm_content_clustering'],
+    zip_safe=False
+)
